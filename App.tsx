008a28be4cfbb3c74573e641fa65794603374a2a
Guilherme da Matta <guilherme.damatta@yahoo.com.br>
2023-07-10 11:27:15 -0300

    created header

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -15,6 +16,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        style="light"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? (<Groups />) : (<Loading />)}
     </ThemeProvider>
   )
